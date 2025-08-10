d3bd37f9985503fc58266914a2e56f7bf5c8ef46
Danil <danilbystrov8@gmail.com>
2025-08-10 23:33:53 +0300

    add redux thunk

--- src/components/control-panel/control-panel.jsx
+++ src/components/control-panel/control-panel.jsx
@@ -1,5 +1,5 @@
 import { useDispatch } from 'react-redux';
-import { increaseAge, RESER_AGE } from '../../actions';
+import { increaseAge, RESER_AGE, changeUserAction } from '../../actions';
 
 export const ControlPanel = () => {
 	const dispatch = useDispatch();
@@ -10,11 +10,15 @@ export const ControlPanel = () => {
 	const onAgeReset = () => {
 		dispatch(RESER_AGE);
 	};
+	const onUserChange = () => {
+		dispatch(changeUserAction);
+	};
 
 	return (
 		<div>
 			<button onClick={onAgeIncrease}> Увеличить возраст </button>
 			<button onClick={onAgeReset}> Сбросить возраст </button>
+			<button onClick={onUserChange}> Сменить пользователя </button>
 		</div>
 	);
 };
--- src/store.jsx
+++ src/store.jsx
@@ -1,9 +1,10 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { userReducer, productsReducer } from './reducers';
+import { thunk } from 'redux-thunk';
 
 const reducer = combineReducers({
 	userState: userReducer,
 	productsState: productsReducer,
 });
 
-export const store = createStore(reducer);
+export const store = createStore(reducer, applyMiddleware(thunk));
