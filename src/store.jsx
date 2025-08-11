bb7fea5e94e55a1196e35150cc0ef841a7b74815
Danil <danilbystrov8@gmail.com>
2025-08-12 00:15:36 +0300

    add redux dev tool

--- src/store.jsx
+++ src/store.jsx
@@ -1,10 +1,12 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { userReducer, productsReducer } from './reducers';
 import { thunk } from 'redux-thunk';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const reducer = combineReducers({
 	userState: userReducer,
 	productsState: productsReducer,
 });
 
-export const store = createStore(reducer, applyMiddleware(thunk));
+export const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
